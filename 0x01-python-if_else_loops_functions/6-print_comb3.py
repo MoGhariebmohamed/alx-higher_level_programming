d8dd5b41f3dc47930741c52da73f9a2b74e51a1a
Mohamed <m.gharieb@gmail.com>
2023-10-31 14:50:16 -0700

    t6 v2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for X in range(0, 9):
-    for Y in range(X +1, 10):
+    for Y in range(X + 1, 10):
         if X == 8 and Y == 9:
-            print("{:d}{:d}".format(X,Y))
+            print("{:d}{:d}".format(X, Y))
         else:
-            print("{:d}{:d}, ".format(X,Y), end="")
+            print("{:d}{:d}, ".format(X, Y), end="")
