549476198a750a0d21815bd6b5bf53eada50eeeb
Mohamed <m.gharieb@gmail.com>
2023-10-31 18:47:42 -0700

    t12 v1

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    Y = range(1,101)
+    for X in Y:
+        if X % 3 == 0 and X % 5 != 0:
+            print("Fizz ", end="")
+        elif X % 5 == 0 and X % 3 != 0:
+            print("Buzz ", end="")
+        elif X % 5 == 0 and X % 3 == 0:
+            print("FizzBuzz ", end="")
+        else:
+            print("{} ".format(X), end="")
+
