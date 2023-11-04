4bbd58f44ba25753eb546aa989072c5fb5dcbfc4
Mohamed <m.gharieb@gmail.com>
2023-11-04 10:45:52 -0700

    t1 v0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for mlist in my_list:
-        print("{:d}".format(mlist))
+def element_at(my_list, idx):
+
+    if idx < 0 or idx > len(my_list):
+        return None
+    x = my_list[idx]
+    return x
---
+++ 0x03-python-data_structures/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+element_at = __import__('1-element_at').element_at
+
+my_list = [1, 2, 3, 4, 5]
+idx1 = 3
+idx2 = -1
+idx3 = 0
+idx4 = 10
+print("Element at index {:d} is {}".format(idx1, element_at(my_list, idx1)))
+print("Element at index {:d} is {}".format(idx2, element_at(my_list, idx2)))
+print("Element at index {:d} is {}".format(idx3, element_at(my_list, idx3)))
+print("Element at index {:d} is {}".format(idx4, element_at(my_list, idx4)))
