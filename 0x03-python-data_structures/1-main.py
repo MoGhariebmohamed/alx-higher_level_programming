4befbe2809ce45be5c4688f46c936c695327adfe
Mohamed <m.gharieb@gmail.com>
2023-11-04 10:48:44 -0700

    t1 v1

--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -5,7 +5,7 @@ my_list = [1, 2, 3, 4, 5]
 idx1 = 3
 idx2 = -1
 idx3 = 0
-idx4 = 10
+idx4 = 5
 print("Element at index {:d} is {}".format(idx1, element_at(my_list, idx1)))
 print("Element at index {:d} is {}".format(idx2, element_at(my_list, idx2)))
 print("Element at index {:d} is {}".format(idx3, element_at(my_list, idx3)))
