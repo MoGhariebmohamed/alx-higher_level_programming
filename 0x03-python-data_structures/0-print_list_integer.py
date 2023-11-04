1630580948c33ab147bd3302d2216e9bd0cafdce
Mohamed <m.gharieb@gmail.com>
2023-11-04 10:13:19 -0700

    t0 v0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for mlist in my_list:
+        print("{:d}".format(mlist))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+this is the fourth task for python to practice lists and data structure
+Python
