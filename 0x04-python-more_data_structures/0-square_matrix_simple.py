a00a97306f7e126cb99e629b7f549b92df4aa962
Mohamed <m.gharieb@gmail.com>
2023-11-10 19:54:11 -0800

    t0 v1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,8 @@
 def square_matrix_simple(matrix=[]):
     new_mat = []
     for i in matrix:
-        sqr_m = lambda y: y ** 2
+        def sqr_m(y):
+            y = y ** 2
+            return y
         new_mat.append(list(map(sqr_m, i)))
     return new_mat
