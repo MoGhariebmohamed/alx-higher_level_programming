59a4f7c2f684800729f3996ceb783cd10435dcaa
mohamed gharieb <m.gharieb89@gmail.com>
2023-11-21 18:55:28 -0800

    t3 v0

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""its for defines a square"""
+
+
+class Square:
+    """for squre dimensions"""
+
+    def __init__(self, size=0):
+        """lets initiate new square
+
+        Args:
+            size : For getting  size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """for the area of the square"""
+            return (self.__size * self.__size)
