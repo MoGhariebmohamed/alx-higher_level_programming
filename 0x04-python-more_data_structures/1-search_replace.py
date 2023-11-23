b6b51768317e2d8bb9941a5bf1bf6bd4746e0204
mohamed gharieb <m.gharieb89@gmail.com>
2023-11-23 05:37:20 -0800

    t1 v6

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for x in range(len(my_list)):
-        if my_list[x] == search:
-            my_list[x] = replace
-            new_list.append(my_list[x])
-        else:
-            new_list.append(my_list[x])
-    return new_list
+    return list(map(lambda E: replace if E == search else E, my_list))
