022f358625d2c9e07cb0311a050eca0f6e7b9862
mohamed gharieb <m.gharieb89@gmail.com>
2023-11-30 11:07:09 -0800

    t100 v3

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string():
+     magic_string.repeater = getattr(magic_string, "repeater", 0) + 1
+     return (", ".join(["BestSchool" for x in range(magic_string.repeater)]))
