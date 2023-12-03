b3ac0c2e359f14832d02b705eaa4c7a0fc98abb8
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-03 14:08:56 -0800

    t2 v5

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,7 +2,7 @@
 def matrix_divided(matrix, div):
     """
     the matrix div function return two matrix_divided
-    
+
     Args:
         matrix: the matrix
         div: the divission value
@@ -13,24 +13,26 @@ def matrix_divided(matrix, div):
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
-    for l in matrix:
-        if type(l) is not list:
+    for le in matrix:
+        if type(le) is not list:
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
         if size is None:
-            size = len(l)
-        elif size != len(l):
+            size = len(le)
+        elif size != len(le):
             raise TypeError("Each row of the matrix must have the same size")
-        for i in l:
+        for i in le:
             if type(i) is not int and type(i) is not float:
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix(
+                    list of lists) of integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    return [[round(i / div, 2) for i in le] for le in matrix]
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
