3e6ec9abe978316f306e9ea3c8afe2ccbce8395f
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-04 17:30:35 -0800

    t1 v0

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this for inherting class"""
+class MyList(list):
+    """
+    inhernt class from list
+    """
+    def print_sorted(self):
+        """
+        method to print sorted list
+        """
+        print(sorted(self))
