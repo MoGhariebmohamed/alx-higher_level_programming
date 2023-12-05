15f802e8ed07a17b14190b416628e0169b27c5e8
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-04 18:17:32 -0800

    t6 v1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module to print class"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    pass
+
+
+def area(self):
+    """
+    this function to raise message
+    Args:
+    return true if ok other False
+    """
+    raise Exception("area() is not implemented")
