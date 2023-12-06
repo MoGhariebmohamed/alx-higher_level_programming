a314c8026d4844867a9b8a334db04a281ed5eb83
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-05 17:42:14 -0800

    t5 v1

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module Write a function that writes an Object to a text file JSON"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    the function to JSON of object
+    Args:
+        my_obj: the object of class
+        filename: name of the file
+    """
+    with open(filename, "w") as x:
+        return (json.dumps(my_obj))
