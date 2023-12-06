6fb4d14fc46832664e29b944c8bb53b5802f7c32
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-05 17:12:38 -0800

    t3 v1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module Write a function that returns the JSON of string object"""
+
+import json
+def to_json_string(my_obj):
+    """
+    the function to JSON of object
+    Args:
+        my_obj: the object of class
+    """
+    return (json.dumps(my_obj))
