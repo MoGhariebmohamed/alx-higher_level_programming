084d2b907bdf474a1b7e7ecd689ad4584948baa3
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-13 20:46:29 -0800

    t1 v2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 class Base:
-"""a base class"""
-
+    """a base class"""
 
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
-         goal of it is to manage id attribute
-         
-         Args:
+        goal of it is to manage id attribute
+
+        Args:
             id: public instance attribute id
         """
-        if id != None:
+
+        if id is not None:
             self.id = id
         else:
-           self.id = Base.__nb_objects + 1
-
-
+            self.id = Base.__nb_objects + 1
