45b485d5f8e190362de33b17c94d170b53738041
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-15 13:29:08 -0800

    t15 v1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,3 +20,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        the static method eturns the JSON string of list_dictionaries
+
+        Args:
+            list_dictionaries: the list dictionary
+        """
+        if list_dictionaries is None or list_dictionaries == 0:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
