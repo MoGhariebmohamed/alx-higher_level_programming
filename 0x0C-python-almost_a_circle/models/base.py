acccaf92d651af20018c267bb9207a276ed95688
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-15 13:39:26 -0800

    t15 v4

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ for base class"""
-
+import json
 
 class Base:
     """a base class"""
@@ -31,4 +31,5 @@ class Base:
         """
         if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
-        return (json.dumps(list_dictionaries))
+        else:
+           return (json.dumps(list_dictionaries))
