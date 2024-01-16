cc3b80908b77ecc6d0ac26321239bf7308f8b176
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-16 10:59:02 -0800

    t15 v2

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
--- a script that updates the score of Bob to 10 in the table second_table
+-- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
 UPDATE second_table SET `score` = 10 WHERE `name` = "Bob";
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score) AS "average "FROM second_table; 
+SELECT `score` , COUNT(*) as `number` FROM second_table GROUP BY `score` ORDER BY `number` DESC;
