d91679ee682b1d648dc6209673071eadafa2f67e
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-16 04:07:05 -0800

    t5 v0

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
--- script that show tacreates a table called first_table in current database.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- script that show to creates a table called first_table in current database.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
