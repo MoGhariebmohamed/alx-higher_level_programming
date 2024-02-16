eeb6ad558fb3e1a6406f1d052e31686d102cdb0d
mohamed gharieb <m.gharieb89@gmail.com>
2024-02-16 12:17:21 -0800

    t12 v1

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (a === 1 || a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+const input1 = process.argv[2];
+if (isNaN(input1)) {
+  console.log(1);
+} else {
+  console.log(factorial(input1));
+}
