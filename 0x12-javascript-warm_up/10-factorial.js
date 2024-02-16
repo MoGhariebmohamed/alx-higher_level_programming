9db1131189b0c3081f6dc70ed72409cfdc1e804a
mohamed gharieb <m.gharieb89@gmail.com>
2024-02-16 11:59:35 -0800

    t10 v1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,15 +3,12 @@ function factorial (a) {
   if (a === 1 || a === 0) {
     return 1;
   } else {
-      return a * factorial(a - 1);
-        }
+    return a * factorial(a - 1);
+  }
 }
-
 const input1 = process.argv[2];
 if (isNaN(input1)) {
   console.log(1);
-}
-else {
+} else {
   console.log(factorial(input1));
 }
-  
