e9392dab9e4b4c13a73d4e81c39277d9760d451a
mohamed gharieb <m.gharieb89@gmail.com>
2024-02-16 15:39:53 -0800

    t0 v0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+def myFunction(username, password, name):
+    """
+    the function to return all methods
+    Args:
+        username: the db username
+        password: db password
+        name: name
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(name), charset="utf8")
+    cur = db.cursor()
+    tablSelect = cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
+
+
+if __name__ == '__main__':
+    myFunction(argv[1], argv[2], argv[3])
