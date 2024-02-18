db0e8cbcc74f24dbff79137a30f4d1b15efed38b
mohamed gharieb <m.gharieb89@gmail.com>
2024-02-18 05:10:36 -0800

    t2 v2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+script that put all states with a name starting N from database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+
+def myFunction(username, password, name):
+    """
+    the function to return all methods
+    Args:
+        username: the db username
+        password: db password
+        name: name
+        state: the searched state
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(name), charset="utf8")
+    cur = db.cursor()
+    tablSelect = cur.execute("SELECT * FROM states WHERE name LIKE BINARY'{}'"
+                             "ORDER BY `id`".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
+
+
+if __name__ == '__main__':
+    myFunction(argv[1], argv[2], argv[3])
