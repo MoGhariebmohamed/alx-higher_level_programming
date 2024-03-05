6965eba1bc0286eadc33052c3670cc8c7128fa25
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-05 13:31:21 -0800

    t6 v2

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" takes in a URL and an email address, sends a POST request """
+
+
+if __name__ == "__main__":
+    from requests importpost
+    from sys import argv
+
+    new_url = argv[1]
+    mail = argv[2]
+    new_req = post(new_url, {'email': mail})
+    print(new_req.text)
