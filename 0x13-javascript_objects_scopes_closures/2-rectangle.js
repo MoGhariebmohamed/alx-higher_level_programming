3e9edd141819db75f7526477a080c1daa947c967
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-05 11:41:10 -0800

    t2 v0

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
-class Rectangle {}
-
+class Rectangle {
 constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
 module.exports = Rectangle;
+
