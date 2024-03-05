32b4176ef8d0d77446c4959daa75265973f4715a
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-05 08:49:00 -0800

    t3 v1

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE "$1" 
+#Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep "allow" | cut -d " " -f 2-
