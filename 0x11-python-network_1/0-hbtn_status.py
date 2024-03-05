3653b043eec06afd8a7322d49bfe232e6ad621cf
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-05 12:33:02 -0800

    t0 v2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(request) as respons:
         bodyRES = respons.read()
         print("Body response:")
         print("\t- type: {}".format(type(bodyRES)))
