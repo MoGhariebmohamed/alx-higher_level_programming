7d25c4689e9e20f8a44c16dad3b5167317832c43
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-26 16:56:33 -0700

    t0v1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-//to read from file
+// to read from file
 
 const filesystem = require('fs');
 filesystem.readFile(process.argv[2], 'utf-8',
-  function (errorr, req_data) {
+  function (errorr, reqdata) {
     if (errorr) {
       console.log(errorr);
       return;
     }
-    console.log(req_data);
+    console.log(reqdata);
   });
