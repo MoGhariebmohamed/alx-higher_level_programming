26645532e5185c0eed40788b60002157a81e433f
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-26 16:51:38 -0700

    t1v1

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/n//to read from file
 //to WRITE from file
 
-const files = require('fs');
+const files = require('files');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+files.writeFile(process.argv[2], process.argv[3], 'utf-8',
   function (errorr) {
     if (errorr) {
       console.log(errorr);
