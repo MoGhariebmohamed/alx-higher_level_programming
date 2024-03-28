f63cccb9ccc73ce391b141d0536f1a9fe7ad44f1
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-28 15:45:45 -0700

    t3vo

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,13 @@
 // to WRITE from file
 
 const request = require('request');
-const urlLink = 'https://swapi-api.alx-tools.com/api/films' + process.argv[2];
+const urlLink = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(urlLink, (errorr, okResponse, content) => {
+request.get(urlLink, function(errorr, okResponse, body) {
   if (errorr) {
     console.log(errorr);	
   } else {
-      const finalData = JSON.parse(content);
-      console.log(finalData.title);
+      const dataShow = JSON.parse(body);
+      console.log(dataShow.title);
   }
 });
